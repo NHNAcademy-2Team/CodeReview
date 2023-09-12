a8bf596b85aa12c483005419e9fcd45a571ab5f0
sseint <sseint@gmail.com>
2023-09-12 18:01:16 +0900

    commit

---
+++ Unit1_Exercise/Information.java
@@ -0,0 +1,31 @@
+public class Information {
+
+    private String city;
+    private String telephoneNum;
+    private String personName;
+
+    public Information(String city, String personName, String telephoneNum) {
+        this.city = city;
+        this.telephoneNum = telephoneNum;
+        this.personName = personName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getTelephoneNum() {
+        return telephoneNum;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+
+    
+    @Override
+    public String toString() {
+        return String.format("%s, %s, %s", city, personName, telephoneNum);
+    }
+    
+}
