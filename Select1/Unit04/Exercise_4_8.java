ead8f48eaca5309f47a8aa7c5a05a2bd95f8fdce
{Hongkyoungseuk} <tug234@daum.net>
2023-09-18 16:36:08 +0900

    test

--- Select1/Unit04/Exercise_4_8.java
+++ Select1/Unit04/Exercise_4_8.java
@@ -6,11 +6,7 @@ public class Exercise_4_8 {
     public static void main(String[] args) {
         int c = 100;
         Account r = new Account(100);
-
-
         Exercise_4_8.method(c,r);
-
-
         System.out.println(c + " " + r.bal);
     }
 }
