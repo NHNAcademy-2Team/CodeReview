f71415cf48bff2da76c9353a195e58b3dd8d8240
Suyeon Soon <soocy0718@gmail.com>
2023-09-18 09:24:11 +0900

    Upload Unit04

--- Unit03/PhoneMessage/SendMessage.java
+++ Unit03/PhoneMessage/SendMessage.java
@@ -3,11 +3,11 @@ package PhoneMessage;
 import java.util.Scanner;
 
 public class SendMessage {
-    public static void send(String[] args) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         String callPhoneNumber = "";
-        System.out.print("code, message를 입력해주세요. 입력 형식) code message : Define (and use) an auxiliary static method for reading a phone number from the keyboard. Solve the exercise so as to use all methods that have been defined in the classes MessageText and Message (possibly by creating and printing various messages).");
+        System.out.print("code, message를 입력해주세요. 입력 형식 ) code message");
         String code = sc.next();
         String m = sc.next();
         MessageText messageText = new MessageText(code, m);
