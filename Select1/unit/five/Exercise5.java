9184c5859f385314429efbb85e9876e375007533
{Hongkyoungseuk} <tug234@daum.net>
2023-09-21 15:58:46 +0900

    0921.15:58

--- Select1/unit/five/Exercise5.java
+++ Select1/unit/five/Exercise5.java
@@ -1,5 +1,22 @@
 package unit.five;
 
 public class Exercise5 {
+    int a;
+    int b;
+    int c;
+
+    public Exercise5(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    public void equation() {
+        double x;
+        double y;
+        x = (-1 * b + Math.sqrt(Math.pow(b,2) - (4 * a * c))) / 2 * a;
+        y = (-1 * b - Math.sqrt(Math.pow(b,2) - (4 * a * c))) / 2 * a;
+        System.out.println("x = " + x + " , " + y);
+    }
 
 }
