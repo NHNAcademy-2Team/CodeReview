0f69a877220d15cd66a835a6d31e902493deb521
hxngji2 <96656059+hxngji2@users.noreply.github.com>
2023-09-21 16:26:21 +0900

    unit5 feedback

--- Select1/unit/five/Exercise1.java
+++ Select1/unit/five/Exercise1.java
@@ -21,24 +21,6 @@ public class Exercise1 {
         }
     }
 
-    // 2
-    public static void triangleType(int length1, int length2, int length3) {
-        int pairs = 0;
-        if (length1 == length2 || length1 == length3 || length2 == length3) {
-            pairs = 1;
-        }
-        if (length1 == length2 && length2 == length3) {
-            pairs = 2;
-        }
-
-        if (pairs == 0) {
-            System.out.println("It is irregular");
-        } else if (pairs == 1) {
-            System.out.println("It is symmetric");
-        } else
-            System.out.println("It is regular");
-    }
-
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         double g = scn.nextDouble();
