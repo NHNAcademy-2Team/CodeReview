8828e3461a728d3cb6293fd18769ea2123fb66fd
{Hongkyoungseuk} <tug234@daum.net>
2023-09-25 10:29:58 +0900

    0925.10:29

--- Select1/src/main/java/unit7/Exercise9.java
+++ Select1/src/main/java/unit7/Exercise9.java
@@ -3,8 +3,7 @@ package src.main.java.unit7;
 public class Exercise9 {
 
     static boolean symmetric(int[][] M) {
-        // M배열이 정사각형(?) 일때 ture를 return;
-        for (int i = 0 ; i < M.length ; i++) {
+        for (int i = 0; i < M.length; i++) {
             if (M.length != M[i].length) {
                 return false;
             }
