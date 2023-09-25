968c372f7786dc5fc4f0832e77f8732dd4443959
{Hongkyoungseuk} <tug234@daum.net>
2023-09-25 13:47:20 +0900

    0925.13:47

--- Select1/src/main/java/unit/seven/Exercise11.java
+++ Select1/src/main/java/unit/seven/Exercise11.java
@@ -1,16 +1,7 @@
 package src.main.java.unit.seven;
 
 public class Exercise11 {
-    /*
-    static boolean diagonal(int[][] M) {
-        for (int i = 2 ; i < M.length ; i++) {
-            if (M[i].length - M[i-1].length != M[i-1].length - M[i-2].length) {
-                return false;
-            }
-        }
-    return true;
-    }
-    */
+
     static boolean diagonal(int[][] Matrix) {
         for (int i = 0; i < Matrix.length; i++) {
             for (int j = 0; j < Matrix[i].length; j++) {
