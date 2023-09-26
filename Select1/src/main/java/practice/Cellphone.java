3b0e4c3241b2bc7d77bf14fb31e954c56c17ffb1
hxngji2 <96656059+hxngji2@users.noreply.github.com>
2023-09-26 13:13:02 +0900

    message commit

---
+++ Select1/src/main/java/practice/Cellphone.java
@@ -0,0 +1,14 @@
+package practice;
+
+public class Cellphone {
+
+    public Message[] messages;
+
+    public Cellphone() {
+        messages = new Message[3];
+
+        messages[0] = new Message("jiwon", "01023344556", "hello");
+    }
+
+
+}
