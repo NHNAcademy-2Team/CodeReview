0ab1d758a4c3f7d080ea2f90752622836e2adf4d
Suyeon Soon <soocy0718@gmail.com>
2023-09-26 13:53:44 +0900

    Upload practice

---
+++ Select1/src/main/java/practice/Cellphone.java
@@ -0,0 +1,10 @@
+package practice;
+
+public class Cellphone {
+
+    public Message[] messages = new Message[1];
+
+    public Cellphone() {
+        this.messages[0] = new Message();
+    }
+}
---
+++ Select1/src/main/java/practice/Message.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class Message {
+    public String sender;
+    public String senderNumber;
+    public String messageText;
+
+    public Message() {
+        this.sender = "수연";
+        this.senderNumber = "01012345678";
+        this.messageText = "Hi";
+    }
+}
