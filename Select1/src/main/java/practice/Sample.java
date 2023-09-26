b3d979bb56059157c7be91b4ba317abd9c6dd813
sseint <sseint@gmail.com>
2023-09-26 15:20:10 +0900

    resolve problem

--- Select1/src/main/java/practice/Sample.java
+++ Select1/src/main/java/practice/Sample.java
@@ -1,7 +1,6 @@
 package practice;
 
 public class Sample {
-
     public static void main(String[] args) {
         Cellphone cellphone = new Cellphone();
         Message message = cellphone.messages[0];
@@ -12,7 +11,6 @@ public class Sample {
 }
 
 class Cellphone {
-
     public Message[] messages;
 
     public Cellphone() {    //생성자에 메소드 호출하는게 더 좋음
@@ -20,12 +18,11 @@ class Cellphone {
     }
 
     public Message[] message() {
-
         this.messages = new Message[3];
 
         String[] sender = {"James", "July", "Tom"};
         String[] senderNumber = {"010-1234-5678", "010-5432-11234", "010-3546-9434"};
-        String[] messageText = {"It's so difficult", "I'm genius", "Hello"};
+        String[] messageText = {"OMG, what are you doing?", "I'm genius", "Hello"};
 
         for (int i = 0; i < 3; i++) {
             this.messages[i] = new Message(sender[i], senderNumber[i], messageText[i]);
@@ -35,15 +32,43 @@ class Cellphone {
 }
 
 class Message {
-
     public String sender;
     public String senderNumber;
     public String messageText;
 
     public Message(String sender, String senderNumber, String messageText) {
         this.sender = sender;
-        this.senderNumber = senderNumber;
-        this.messageText = messageText;
+        this.senderNumber = substractDash(senderNumber);
+        this.messageText = change(messageText);
     }
 
+    public String substractDash(String senderNumber) {
+//        return senderNumber.replaceAll("-", "");
+
+        int index = senderNumber.indexOf("-");
+        StringBuilder sb = new StringBuilder();
+        sb.append(senderNumber.substring(0, 3));    //010
+
+        int index2 = senderNumber.indexOf("-", index + 1);
+
+        sb.append(senderNumber.substring(index+1, index2));
+        sb.append(senderNumber.substring(index2 + 1));
+
+        return sb.toString();
+    }
+
+    public String change(String messageText) {
+        String result = messageText;
+
+        String[] replaceKey = {"R U", "OMG", "WTH"};
+        String[] replaceValue = {"Are you", "Oh my god", "What the hell"};
+
+        for(int i=0; i<replaceKey.length; i++) {
+            if (messageText.contains(replaceKey[i])) {
+                result = messageText.replace(replaceKey[i], replaceValue[i]);
+            }
+        }
+
+        return result;
+    }
 }
