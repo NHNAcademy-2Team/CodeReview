cf52fae1989fbeea44dac09ffa17202f3c17abc6
hxngji2 <96656059+hxngji2@users.noreply.github.com>
2023-09-26 14:58:20 +0900

    Second commit

--- Select1/src/main/java/practice/Cellphone.java
+++ Select1/src/main/java/practice/Cellphone.java
@@ -7,8 +7,23 @@ public class Cellphone {
     public Cellphone() {
         messages = new Message[3];
 
-        messages[0] = new Message("jiwon", "01023344556", "hello");
+        messages[0] = new Message("jiwon", "010-2334-4556", "RY");
+        messages[1] = new Message("boram", "010-4953-2345", "OMG");
+        messages[2] = new Message("namhee", "010-3423-3422", "WTH");
     }
+
+    public static String switchWord(String messageText) {
+        if (messageText == "RY") {
+            messageText = "are you";
+        } else if (messageText == "OMG") {
+            messageText = "oh my god";
+        } else if (messageText == "WTH") {
+            messageText = "what the hell";
+        }
+
+        return messageText;
+    }
+
 
 
 }
--- Select1/src/main/java/practice/Sample.java
+++ Select1/src/main/java/practice/Sample.java
@@ -5,14 +5,15 @@ public class Sample {
     public static void main(String[] args) {
         Cellphone cellphone = new Cellphone();
 
-        Message message = cellphone.messages[0];
 
-        System.out.println(message.sender);
-        System.out.println(message.senderNumber);
-        System.out.println(message.messageText);
+        for (int i = 0; i < cellphone.messages.length; i++) {
+            Message message = cellphone.messages[i];
+            System.out.println(message.sender);
+            System.out.println(message.senderNumber.replaceAll("-", ""));
+            System.out.println(Cellphone.switchWord(message.messageText));
+            System.out.println();
+        }
 
 
     }
-
-
 }
