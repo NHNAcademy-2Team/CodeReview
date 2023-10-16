1059c20c4b1ce4353ebd5b14ae044a1ed37bf64f
th915 <97163658+th915@users.noreply.github.com>
2023-10-16 16:33:24 +0900

    Unit 7 update

--- Select1/src/main/java/unit/seven/Apartment.java
+++ Select1/src/main/java/unit/seven/Apartment.java
@@ -1,43 +1,52 @@
-package unit.seven;
+package seven;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Arrays;
 
 public class Apartment {
-    /**Exercise 12
-     * */
+    /**
+     * Exercise 12
+     * 
+     * Unit 8 Exercise 8
+     * saveToFile, readFromFile, printApartment method
+     */
     private int size;
     private String address;
     private String[] people;
     private int count = 0;
 
-    public Apartment(int size, String address){
+    public Apartment(int size, String address) {
         this.size = size;
         this.address = address;
         this.people = new String[10];
     }
 
-    public String houseOwner(int n){
-        if(n < 0){
+    public String houseOwner(int n) {
+        if (n < 0) {
             throw new IllegalArgumentException("음이 아닌 정수를 입력해주세요.");
-        } else if(n >= 10){
+        } else if (n >= 10) {
             return null;
         } else {
             return people[n];
         }
     }
 
-    public void removePeople(int n){
-        if(n < count){
-            for (int i = n; i < count - 1 ; i++) {
-                people[n] = people[n+1];
+    public void removePeople(int n) {
+        if (n < count) {
+            for (int i = n; i < count - 1; i++) {
+                people[i] = people[i + 1];
             }
             count--;
             people[count] = null;
         }
     }
 
-    public void setPeople(String name){
-        if(count < 10){
+    public void setPeople(String name) {
+        if (count < 10) {
             people[count] = name;
             count++;
         } else {
@@ -45,19 +54,74 @@ public class Apartment {
         }
     }
 
-    public int getSize(){
+    public int getSize() {
         return this.size;
     }
 
-    public String getAddress(){
+    public String getAddress() {
         return this.address;
     }
 
-    public String[] getPeople(){
+    public String[] getPeople() {
         return this.people;
     }
 
-    public String toString(){
+    public String toString() {
         return "아파트 주소 : " + getAddress() + "\n아파트 크기 : " + getSize() + "\n아파트 입주자 : " + Arrays.toString(getPeople());
+    }
+
+    // Unit 8 Exercise 8
+    public void saveToFile(String fileName) throws IOException {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(fileName))) {
+
+            bw.write("size " + this.size + "\n");
+            bw.write("address " + this.address + "\n");
+            for (int i = 0; i < count; i++) {
+                bw.write(i + "번지 " + this.people[i] + "\n");
+            }
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    // Unit 8 Exercise 8
+    public static Apartment readFromFile(BufferedReader br) throws IOException {
+        Apartment apartment;
+        String data;
+
+        if (!br.ready())
+            throw new FileNotFoundException("파일이 존재하지 않습니다.");
+
+        int size = 0;
+        String address = "";
+
+        data = br.readLine();
+        String[] sentence = data.split(" ");
+        size = Integer.parseInt(sentence[1]);
+
+        data = br.readLine();
+        sentence = data.split(" ");
+        address = sentence[1];
+
+        apartment = new Apartment(size, address);
+
+        try {
+            while (br.ready()) {
+                data = br.readLine();
+                sentence = data.split(" ");
+
+                apartment.setPeople(sentence[1]);
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        return apartment;
+    }
+
+    // Unit 8 Exercise 8
+    public static void printApartment(Apartment apartment) {
+        System.out.println(apartment.toString());
     }
 }
--- Select1/src/main/java/unit/seven/Exercise12.java
+++ Select1/src/main/java/unit/seven/Exercise12.java
@@ -1,21 +1,29 @@
-package unit.seven;
+package seven;
+
+import java.io.IOException;
+
 public class Exercise12 {
     /**
      *
-     * Exercise 07.12 Realize a Java class Apartment, whose objects maintain the following information: an integer that
-     * indicates the size in square meters of the apartment, the address, and a list of maximal 10 names of persons that
-     * live in the apartment. Each person living in the apartment has an associated number between 0 and the number
-     * of persons currently living in the apartment minus 1. The class should export the following functionalities:
+     * Exercise 07.12 Realize a Java class Apartment, whose objects maintain the
+     * following information: an integer that
+     * indicates the size in square meters of the apartment, the address, and a list
+     * of maximal 10 names of persons that
+     * live in the apartment. Each person living in the apartment has an associated
+     * number between 0 and the number
+     * of persons currently living in the apartment minus 1. The class should export
+     * the following functionalities:
      *
      * https://www.inf.unibz.it/~calvanese/teaching/04-05-ip/lecture-notes/uni07/node24.html
-     * */
+     */
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         Apartment apartment = new Apartment(100, "조선대학교");
 
         apartment.setPeople("홍길동");
         apartment.setPeople("김철수");
         apartment.setPeople("고봉민");
+        apartment.setPeople("양진영");
 
         System.out.println("2번지에 사는 사람 : " + apartment.houseOwner(2));
 
@@ -23,5 +31,7 @@ public class Exercise12 {
 
         System.out.println("\n아파트 정보\n--------------------");
         System.out.println(apartment);
+
+        apartment.saveToFile("/Users/taehee/과정1/unit/resources/seven/apartment.txt");
     }
 }
