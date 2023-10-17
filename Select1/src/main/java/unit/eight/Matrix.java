2ceb17bc39e13fae8ab5fa296b75459724c82894
sseint <sseint@gmail.com>
2023-10-17 16:53:17 +0900

    Exercise 8.8 update

--- Select1/src/main/java/unit/eight/Matrix.java
+++ Select1/src/main/java/unit/eight/Matrix.java
@@ -1,7 +1,9 @@
 package unit.eight;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 
@@ -10,7 +12,6 @@ public class Matrix {
     private int n;
     private int m;
     private int[][] size;
-    private String filename;
 
     public Matrix(int n, int m) {
 
@@ -18,15 +19,16 @@ public class Matrix {
     }
 
     public void save(String filename) {
-        this.filename = filename;
         File file = new File("/Users/nhn/Desktop/Git/CodeReview/Select1/src/main/resources/" + filename);
 
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(file));) {
 
             for (int i = 0; i < size.length; i++) {
                 for (int j = 0; j < size[0].length; j++) {
-                    bw.write(size[i][j]);
+
+                    bw.write(size[i][j] + " ");
                 }
+                bw.write("\r\n");
             }
 
         } catch (IOException e) {
@@ -35,6 +37,65 @@ public class Matrix {
     }
 
     public static Matrix read(String filename) {
+        File file = new File("/Users/nhn/Desktop/Git/CodeReview/Select1/src/main/resources/" + filename);
+        String[] value;
+        Matrix matrix = null;
+
+        try (BufferedReader br = new BufferedReader(new FileReader(file));) {
+            matrix = sizeMatrix(br);
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                value = line.split(" ");
+                for (int i = 0; i < matrix.getMatrix().length; i++) {
+                    for (int j = 0; j < matrix.getMatrix()[0].length; j++) {
+                        matrix.setMatrix(i, j, Integer.valueOf(value[j]));
+                    }
+                }
+            }
+
+        } catch (IOException e) {
+            System.out.println("잘못된 입력입니다.");
+        }
+
+        return matrix;
+    }
+
+    private static Matrix sizeMatrix(BufferedReader br) {
+        String line;
+        int row = 0;
+        int column = 0;
+        String[] value;
+
+        try {
+            while ((line = br.readLine()) != null) {
+                row++;
+                value = line.split(" ");
+                column = value.length;
+            }
+
+        } catch (IOException e) {
+            System.out.println("잘못된 입력입니다.");
+        }
+
+        Matrix matrix = new Matrix(row, column);
+        return matrix;
+    }
+
+    public void setMatrix(int row, int column, int value) {
+        this.size[row][column] = value;
+    }
+
+    public int[][] getMatrix() {
+        return this.size;
+    }
+
+    public Matrix sum(Matrix m) {
 
     }
+
+    public Matrix product(Matrix m) {
+
+    }
+
 }
