239d8236b3d8b1afbc853c8532a7427e405f410d
{Hongkyoungseuk} <tug234@daum.net>
2023-10-20 09:37:37 +0900

    uint9

---
+++ Select1/src/main/java/unit/nine/Derived.java
@@ -0,0 +1,12 @@
+package unit.nine;
+
+public class Derived extends Base {
+    public Derived() {
+        super();
+        infod = "I am an object of the Derived class";
+    }
+    public String getInfo() {
+        return infod + ", " + super.getInfo();
+    }
+    private String infod;
+} 
