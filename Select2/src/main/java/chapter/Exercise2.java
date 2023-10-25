0b12cb60bc657901038a69c80be55550c95f941d
sseint <sseint@gmail.com>
2023-10-25 09:18:32 +0900

    Exercise2 modified

--- Select2/src/main/java/chapter/Exercise2.java
+++ Select2/src/main/java/chapter/Exercise2.java
@@ -11,13 +11,17 @@ public class Exercise2 {
 
         System.out.println("The first die comes up " + first);
         System.out.println("The second die comes up " + second);
-        System.out.println("Your total roll is " + total);
+        System.out.println("Your total roll is " + total(first, second));
 
     }
 
     public static int roll() {
         int dice = (int)(Math.random() * 6) + 1;
         return dice;
+    }
+
+    public static int total(int first, int second) {
+        return first + second;
     }
 }
 
