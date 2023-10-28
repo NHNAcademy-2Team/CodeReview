31380eef7f106f87c99b16eb8473c501abc26c77
Suyeon Soon <soocy0718@gmail.com>
2023-10-29 02:21:45 +0900

    Update Chapter3

--- Select2/src/main/java/chapter/three/exercise4/Exercise4.java
+++ Select2/src/main/java/chapter/three/exercise4/Exercise4.java
@@ -5,12 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Exercise4 {
-    private static final String pattern = "[{}\\[\\]/?.,;:|)*~`!^\\-_+<>@#$%&=('\"]";
+    private static final String SPEACIALCHARACTER
+            = "[{}\\[\\]/?.,;:|)*~`!^\\-_+<>@#$%&=('\"]";
 
     public static void main(String[] args) {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             String line = br.readLine();
-            System.out.println(line.replaceAll(pattern, "").replaceAll("\\s", "\n"));
+            System.out.println(line.replaceAll(SPEACIALCHARACTER, "").replaceAll("\\s", "\n"));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
