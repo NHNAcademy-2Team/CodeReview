fd263af99007b391d14b503c96d6cf9487c7255c
sseint <sseint@gmail.com>
2023-10-30 08:55:55 +0900

    update

--- Select2/src/main/java/chapter/three/Exercise2.java
+++ Select2/src/main/java/chapter/three/Exercise2.java
@@ -1,8 +1,52 @@
 package chapter.three;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Exercise2 {
 
     //1과 10000 사이의 정수 중 어느 정수가 가장 많은 수의 약수들을 가지고 있는가, 그리고 그 정수는 몇 개의 약수들을 가지는가?
     //여러 정수들은 약수들의 최대 갯수가 동일할 수 있다. 프로그램은 이들 중 하나만 출력해야 한다.
+
+    private static int max;
+    private static List<Integer> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+
+        count(1, 10000);
+        System.out.println(max());
+
+    }
+
+    public static void count(int start, int end) {
+        for (int i = start; i < end; i++) {
+            yaksu(i);
+        }
+    }
+
+    public static void yaksu(int num) {
+
+        int count = 0;
+
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                count++;
+            }
+        }
+        list.add(count);
+    }
+
+    public static int max() {
+        int max = 0;
+        for (int i =0; i<list.size(); i++) {
+            if (max < list.get(i)) {
+                max = list.get(i);
+            }
+        }
+        return max;
+    }
+
+
+
 
 }
