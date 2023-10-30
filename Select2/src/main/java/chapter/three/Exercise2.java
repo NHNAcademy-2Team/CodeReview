dee885dffb0851b1e6c639241472f25064b5bb8a
sseint <sseint@gmail.com>
2023-10-30 09:50:29 +0900

    Exercise3 update

--- Select2/src/main/java/chapter/three/Exercise2.java
+++ Select2/src/main/java/chapter/three/Exercise2.java
@@ -1,15 +1,19 @@
 package chapter.three;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Exercise2 {
 
     //1과 10000 사이의 정수 중 어느 정수가 가장 많은 수의 약수들을 가지고 있는가, 그리고 그 정수는 몇 개의 약수들을 가지는가?
     //여러 정수들은 약수들의 최대 갯수가 동일할 수 있다. 프로그램은 이들 중 하나만 출력해야 한다.
+    //보류
 
     private static int max;
     private static List<Integer> list = new ArrayList<>();
+    private static Map<Integer, Integer> map = new HashMap<>();
 
     public static void main(String[] args) {
 
@@ -33,20 +37,18 @@ public class Exercise2 {
                 count++;
             }
         }
-        list.add(count);
+        map.put(num, count);
     }
 
     public static int max() {
         int max = 0;
-        for (int i =0; i<list.size(); i++) {
-            if (max < list.get(i)) {
-                max = list.get(i);
+        for (int i = 0; i < map.size(); i++) {
+            if (max < map.get(i)) {
+                max = map.get(i);
             }
         }
         return max;
     }
-
-
 
 
 }
