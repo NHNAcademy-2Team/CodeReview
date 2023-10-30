b5db9e1a988fde546e088956ef74cf9c9ee4a09e
sseint <sseint@gmail.com>
2023-10-30 10:48:41 +0900

    Exercise2 update

--- Select2/src/main/java/chapter/three/Exercise2.java
+++ Select2/src/main/java/chapter/three/Exercise2.java
@@ -1,35 +1,43 @@
 package chapter.three;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 public class Exercise2 {
 
     //1과 10000 사이의 정수 중 어느 정수가 가장 많은 수의 약수들을 가지고 있는가, 그리고 그 정수는 몇 개의 약수들을 가지는가?
     //여러 정수들은 약수들의 최대 갯수가 동일할 수 있다. 프로그램은 이들 중 하나만 출력해야 한다.
     //보류
 
-    private static int max;
-    private static List<Integer> list = new ArrayList<>();
-    private static Map<Integer, Integer> map = new HashMap<>();
-
     public static void main(String[] args) {
 
-        count(1, 10000);
-        System.out.println(max());
+        Yaksu yaksu = new Yaksu(1, 10000);
+        System.out.println(yaksu.toString());
 
     }
 
-    public static void count(int start, int end) {
-        for (int i = start; i < end; i++) {
-            yaksu(i);
+
+}
+
+class Yaksu {
+
+    private int start;
+    private int end;
+    private int num;
+    private int max;
+
+
+    public Yaksu(int start, int end) {
+        this.start = start;
+        this.end = end;
+        count();
+    }
+
+    public void count() {
+
+        for (int i = this.start; i <= this.end; i++) {
+            max(i, yaksu(i));
         }
     }
 
-    public static void yaksu(int num) {
-
+    public int yaksu(int num) {
         int count = 0;
 
         for (int i = 1; i <= num; i++) {
@@ -37,18 +45,17 @@ public class Exercise2 {
                 count++;
             }
         }
-        map.put(num, count);
+        return count;
     }
 
-    public static int max() {
-        int max = 0;
-        for (int i = 0; i < map.size(); i++) {
-            if (max < map.get(i)) {
-                max = map.get(i);
-            }
+    public void max(int num, int count) {
+        if (max < count) {
+            this.max = count;
+            this.num = num;
         }
-        return max;
     }
 
-
+    public String toString() {
+        return this.num + " : " + this.max;
+    }
 }
