43a67ca2ac19066a02671649625eec3432049cc5
sseint <sseint@gmail.com>
2023-11-08 09:24:15 +0900

    feedback

--- Select2/src/main/java/chapter/four/Exercise2.java
+++ Select2/src/main/java/chapter/four/Exercise2.java
@@ -14,7 +14,7 @@ public class Exercise2 {
 
             System.out.print("> ");
             String str = br.readLine();
-            hexToInt(str);
+            hexToDecimal(str);
 
         } catch (IOException e) {
             e.printStackTrace();
@@ -22,7 +22,7 @@ public class Exercise2 {
 
     }
 
-    public static void hexToInt(String str) {
+    public static void hexToDecimal(String str) {
 
         int value = 0;
         for (int i = 0; i < str.length(); i++) {
@@ -32,34 +32,29 @@ public class Exercise2 {
         System.out.println(value);
     }
 
-    public static char hexValue(char ch) {
+    public static int hexValue(char ch) {
 
         ch = Character.toUpperCase(ch);
 
         switch(ch) {
-            case 0 :
-            case 1 :
-            case 2 :
-            case 3 :
-            case 4 :
-            case 5 :
-            case 6 :
-            case 7 :
-            case 8 :
-            case 9 :
-                return ch;
+            case '0' :
+            case '1' :
+            case '2' :
+            case '3' :
+            case '4' :
+            case '5' :
+            case '6' :
+            case '7' :
+            case '8' :
+            case '9' :
+                return (int) (ch - '0');
             case 'A' :
-                return 10;
             case 'B' :
-                return 11;
             case 'C' :
-                return 12;
             case 'D' :
-                return 13;
             case 'E' :
-                return 14;
             case 'F' :
-                return 15;
+                return (int) (ch - 'A' + 10);
             default:
                 throw new IllegalArgumentException();
         }
